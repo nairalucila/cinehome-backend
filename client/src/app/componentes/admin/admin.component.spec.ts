7054ff8e33527a8e9d80512aab8d0763a77b9644
nairalucila <nairacoppari@gmail.com>
2022-01-25 21:28:32 -0300

    tests funcionando

--- client/src/app/componentes/admin/admin.component.spec.ts
+++ client/src/app/componentes/admin/admin.component.spec.ts
@@ -1,13 +1,19 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { AdminComponent } from './admin.component';
 
 describe('AdminComponent', () => {
   let component: AdminComponent;
   let fixture: ComponentFixture<AdminComponent>;
+  const initialState = {};
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule,MatSnackBarModule ],
+      providers: [provideMockStore({ initialState })],
       declarations: [ AdminComponent ]
     })
     .compileComponents();
