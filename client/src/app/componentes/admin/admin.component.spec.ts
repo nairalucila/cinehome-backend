48b422582aa5a5db9a4b49caf4b0768bdad9d6d0
nairalucila <nairacoppari@gmail.com>
2022-01-27 10:16:00 -0300

    se agregaron test pero no funcionan, yupi

--- client/src/app/componentes/admin/admin.component.spec.ts
+++ client/src/app/componentes/admin/admin.component.spec.ts
@@ -2,21 +2,31 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { provideMockStore } from '@ngrx/store/testing';
+import { UsuariosService } from 'src/app/servicios/usuarios.service';
 
 import { AdminComponent } from './admin.component';
+
+import { lista } from './listausuariosdata';
 
 describe('AdminComponent', () => {
   let component: AdminComponent;
   let fixture: ComponentFixture<AdminComponent>;
   const initialState = {};
+  let mockUsuariosService: jasmine.SpyObj<UsuariosService>;
+  let listaUsuarios = lista;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule,MatSnackBarModule ],
-      providers: [provideMockStore({ initialState })],
-      declarations: [ AdminComponent ]
-    })
-    .compileComponents();
+      imports: [HttpClientTestingModule, MatSnackBarModule],
+      providers: [
+        provideMockStore({ initialState }),
+        {
+          provide: UsuariosService,
+          useValue: jasmine.createSpyObj('UsuariosService', ['traerUsuarios']),
+        },
+      ],
+      declarations: [AdminComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -25,7 +35,30 @@ describe('AdminComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  // it('should create a button', () => {
+  // //  spyOn(component.traerListaUsuarios, 'click');
+
+  //   let button = fixture.debugElement.nativeElement.querySelector('traerUs');
+  //   button.onclick();
+
+  //   fixture.whenStable().then(() => {
+  //     expect(component.traerListaUsuarios).toEqual(listaUsuarios);
+  //   });
+  //   //expect(component).toBeTruthy();
+  // });
+
+  it('should test a button', () => {
+    const traerListaUsuarios: any = component.traerListaUsuarios();
+    const spy = spyOn(traerListaUsuarios, 'listaUsuarios').and.returnValue(
+      listaUsuarios
+    );
+
+    const usuarios = traerListaUsuarios;
+    expect(usuarios).toBe(listaUsuarios);
+    expect(spy).toHaveBeenCalled();
   });
+//https://www.youtube.com/watch?v=ubVHEwmmr-E
+  // it('Muestra una lista de usuarios', () => {
+  //   mockUsuariosService.traerUsuarios;
+  // });
 });
